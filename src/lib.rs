554fb5162d17713c38a0494e7fe389fc9421f62d
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-10-02 19:19:56 +0200

    Adds basic suggestions repository code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn make_empty_suggestions_repository() -> SuggestionsRepository {
+    SuggestionsRepository {
+        suggestions: Vec::new(),
+    }
+}
+
+pub struct SuggestionsRepository {
+    suggestions: Vec<String>, 
+}
+
+impl SuggestionsRepository {
+    pub fn get_suggestion_by_index(&self, index: usize) -> &str {
+        &self.suggestions[index]
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.suggestions.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.suggestions.len()
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +27,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn empty_repository() {
+        let db = make_empty_suggestions_repository();
+        assert!(db.is_empty());
+    }
+
+    #[test]
+    fn empty_repository_size() {
+        let db = make_empty_suggestions_repository();
+        assert_eq!(db.len(), 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_on_empty_repository() {
+        let db = make_empty_suggestions_repository();
+
+        let _suggestion = db.get_suggestion_by_index(0);
     }
 }
