57ff3aaa3e5d9f25415f70f1bfd4770621749f0c
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-10-03 16:09:49 +0200

    Adds new() as the main factory method

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub fn make_empty_suggestions_repository() -> SuggestionsRepository {
-    SuggestionsRepository {
-        suggestions: Vec::new(),
-    }
-}
-
 pub struct SuggestionsRepository {
     suggestions: Vec<String>, 
 }
 
 impl SuggestionsRepository {
+    pub fn new() -> SuggestionsRepository {
+        SuggestionsRepository {
+            suggestions: Vec::new(),
+        }
+    }
+
     pub fn get_suggestion_by_index(&self, index: usize) -> &str {
         &self.suggestions[index]
     }
@@ -28,20 +28,20 @@ mod tests {
 
     #[test]
     fn empty_repository() {
-        let db = make_empty_suggestions_repository();
+        let db = SuggestionsRepository::new();
         assert!(db.is_empty());
     }
 
     #[test]
     fn empty_repository_size() {
-        let db = make_empty_suggestions_repository();
+        let db = SuggestionsRepository::new();
         assert_eq!(db.len(), 0);
     }
 
     #[test]
     #[should_panic]
     fn panic_on_empty_repository() {
-        let db = make_empty_suggestions_repository();
+        let db = SuggestionsRepository::new();
 
         let _suggestion = db.get_suggestion_by_index(0);
     }
